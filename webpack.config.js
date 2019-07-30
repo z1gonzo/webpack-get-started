848c887016b5b804606d6e67316b354607a16f2f
z1gonzo <goonsky@gmail.com>
2019-07-30 23:40:58 +0200

    npm i -D webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    module: {
+        mode: 'development',
+
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "src/index.html",
+            filename: "./index.html"
+        }),
+    ]
+}
