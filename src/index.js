87b9d102af9c3d1bc5d9dd95757c3ffa6cda183d
z1gonzo <goonsky@gmail.com>
2019-08-08 20:39:17 +0200

    npm i -D mini-css-extract-plugin, optimize-css-assets-webpack-plugin...

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import { hello } from './hello';
 import "./main.scss";
+// import navBar from './navBar';
 
 console.log(hello('tak'))
+
+const klik = document.getElementById("kliker");
+
+console.log("Witam")
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,61 @@
 const path = require('path');
-const webpack = require('webpack');
+// const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     mode: 'development',  // production
     devtool: "none",  //eval gone
     devServer: {
         contentBase: './src',
-        // watchContentBase: true,
+        open: true,
         hot: true,
       },
-    entry: './src/index.js',  
+
+    entry: {
+       main:  './src/index.js',
+       vendor:  './src/vendor.js'
+    },  
     output: {
-      filename: 'main.js',
+      filename: '[name]-[hash:8].js',
       path: path.resolve(__dirname, 'dist')
     },
+
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserPlugin()
+        ]
+    },
+
+    plugins: [
+        new HtmlWebPackPlugin({
+            filename: "index.html",
+            template: "./src/index.html"
+        }),
+
+        new MiniCssExtractPlugin({ 
+            filename: "[name]-[hash:8].css"
+        }),
+
+        new CleanWebpackPlugin(),
+
+        // We can add new HTML page here
+
+        // Needed to enable HMR
+    //   new webpack.HotModuleReplacementPlugin()
+    ],
 
     module: {
         rules: [
             {
-                test: /\.scss$/,
+                test: /\.(scss|sass)$/,
                 use: [
-                    "style-loader", // 3. Injects styles into DOM
+                    MiniCssExtractPlugin.loader, // 3. Extract css into files
+                    // "style-loader", // 3. Injects styles into DOM
                     "css-loader", // 2. Turns css into commonjs
                     "sass-loader" // 1. Turns sass into css
                 ]
@@ -31,18 +65,25 @@ module.exports = {
                 use: [
                     {
                         loader: "html-loader",
-                        options: { minimize: true }
+                        options: { 
+                            minimize: false // true for prod
+                         }
                     }
                 ]
-            }
+            },
+            {
+                test: /\.(png|jpeg|jpg|gif|svg)$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                    //   publicPath: './images',
+                      outputPath: './images',
+                      name: '[name].[ext]'
+                    }
+                  }
+                ]
+            },
         ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            filename: "index.html",
-            template: "./src/index.html"
-        }),
-        // Needed to enable HMR
-    //   new webpack.HotModuleReplacementPlugin()
-    ]
+    }
 }
